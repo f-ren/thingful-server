f1f45af3be89ab491effb121d7f27912ff39baf3
Ren F <renfinn89@gmail.com>
2020-07-03 10:32:21 -0700

    registration

---
+++ src/auth/user/user-service.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs');
+
+const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
+
+const UserService = {
+  hasUserWithUserName(db, username) {
+    return db('thingful_users')
+      .where({ username })
+      .first()
+      .then((user) => !!user);
+  },
+  insertUser(db, newUser) {
+    return db
+      .insert(newUser)
+      .into('thingful_users')
+      .returning('*')
+      .then(([user]) => user);
+  },
+  validatePassword(password) {
+    if (password.length < 8) {
+      return 'Password be longer than 8 characters';
+    }
+    if (password.startsWith(' ') || password.endsWith(' ')) {
+      return 'Password must not start or end with empty spaces';
+    }
+    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
+      return 'Password must contain one upper case, lower case, number and special character';
+    }
+    return null;
+  },
+  hashPassword(password) {
+    return bcrypt.hash(password, 12);
+  },
+  serializeUser(user) {
+    return {
+      id: user.id,
+      full_name: user.name,
+      nickname: user.nickname,
+      user_name: user.username,
+    };
+  },
+};
+
+module.exports = UserService;
